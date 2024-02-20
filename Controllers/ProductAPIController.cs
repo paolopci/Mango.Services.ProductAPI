06248fa21e370abc07e055afcb85c4ab1e449ff5
PaoloPci <paolopci@yahoo.it>
2024-02-20 15:40:53 +0100

    complete ProductApiController

--- Controllers/ProductAPIController.cs
+++ Controllers/ProductAPIController.cs
@@ -54,5 +54,60 @@ namespace Mango.Services.ProductAPI.Controllers
             }
             return _response;
         }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product obj = _db.Products.First(p => p.ProductId == id);
+                _db.Products.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
