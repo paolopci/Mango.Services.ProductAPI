87016686e24fa5994a832b73c75b1a442cbbeebf
PaoloPci <paolopci@yahoo.it>
2024-02-20 15:03:23 +0100

    Add Response Dto

--- Controllers/ProductAPIController.cs
+++ Controllers/ProductAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ProductAPI.Controllers
@@ -7,5 +9,26 @@ namespace Mango.Services.ProductAPI.Controllers
     [ApiController]
     public class ProductAPIController : ControllerBase
     {
+        private readonly AppDbContext _db;
+
+        public ProductAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Product> objList = _db.Products.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
     }
 }
