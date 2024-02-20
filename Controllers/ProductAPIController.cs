a6b509a1cdc997a4ac318ad695673420f8ad2159
PaoloPci <paolopci@yahoo.it>
2024-02-20 12:39:05 +0100

    add productApiController and setting port:7000

---
+++ Controllers/ProductAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+    }
+}
